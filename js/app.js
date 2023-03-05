ebbd12343edf39486a0990040ef454ba6d9e3ce0
Md. Jahirul Islam <jahirulislam12283@gmail.com>
2023-03-05 15:25:44 +0600

    spinner added

--- js/app.js
+++ js/app.js
@@ -40,17 +40,30 @@ const displayPhones = phones => {
 
     })
 
+    // stop loader
+    toggleSpinner(false);
+a
+    
 
 }
 
-
+// handle search btn click
 document.getElementById('btn-search').addEventListener('click', function () {
+    // start loader
+    toggleSpinner(true);
     const searchField = document.getElementById("search-field");
-
     const searchText = searchField.value;
-
     loadPhones(searchText)
 
 })
 
+const toggleSpinner = isLoading => {
+    const loaderSection = document.getElementById('loader')
+    if(isLoading){
+        loaderSection.classList.remove("d-none")
+    }
+    else{
+        loaderSection.classList.add("d-none")  
+    }
+}
 loadPhones()
